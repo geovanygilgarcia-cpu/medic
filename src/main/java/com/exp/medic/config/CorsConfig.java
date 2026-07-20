1a42480184341508a8d434da580260b8d8d0574c
geovanygilgarcia-cpu <geovany.gil.garcia@gmail.com>
2026-07-20 13:09:29 -0600

    configuracion de cors

--- src/main/java/com/exp/medic/config/CorsConfig.java
+++ src/main/java/com/exp/medic/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://expmedic-front.onrender.com/**")
+                .allowedOrigins("https://expmedic-front.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
