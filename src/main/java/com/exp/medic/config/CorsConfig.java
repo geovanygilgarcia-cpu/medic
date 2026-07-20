60742fede6fa0c03d1e8bb8620ff241c2181114c
geovanygilgarcia-cpu <geovany.gil.garcia@gmail.com>
2026-07-20 16:25:45 -0600

    correccion crosss 2 uris

--- src/main/java/com/exp/medic/config/CorsConfig.java
+++ src/main/java/com/exp/medic/config/CorsConfig.java
@@ -10,8 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://expmedic-front.onrender.com")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins(
+                        "https://expmedic-front.onrender.com",
+                        "http://localhost:4200"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
